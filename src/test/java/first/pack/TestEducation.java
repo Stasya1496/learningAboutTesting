0aa89ee91d499457974c899a695b46f73077952c
Anastasiya <mudryi-kaa@yandex.ru>
2022-08-12 15:30:29 +0300

    This is commit in master (1 stage)

---
+++ src/test/java/first/pack/TestEducation.java
@@ -0,0 +1,11 @@
+package first.pack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestEducation {
+    @Test
+    void methodFirst() {
+        Assertions.assertTrue(true);
+    }
+}
